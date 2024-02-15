5a398cfac59b4a1ead9e45122a89b0f2cf0d55d5
TAEHA117 <nn_goods@naver.com>
2024-02-15 17:18:36 +0900

    Test

---
+++ src/page_03/stage_4/Sample_62.java
@@ -0,0 +1,8 @@
+package page_03.stage_4;
+
+public class Sample_62 {
+    public static void main(String[] args) {
+        String a = "Hello Java";
+        System.out.println(a.replaceAll("Java", "World")); // Hello World를 출력
+    }
+}
