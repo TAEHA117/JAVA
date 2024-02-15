5946dc40a3d058117dc1c07249f8a3f878595836
TAEHA117 <nn_goods@naver.com>
2024-02-15 17:29:05 +0900

    Test

--- src/page_03/stage_4/java/Sample_62.java
+++ src/page_03/stage_4/java/Sample_62.java
@@ -2,7 +2,10 @@ package page_03.stage_4.java;
 
 public class Sample_62 {
     public static void main(String[] args) {
+        /*String a = "Hello Java";
+        System.out.println(a.replaceAll("Java", "World")); // Hello World를 출력*/
+
         String a = "Hello Java";
-        System.out.println(a.replaceAll("Java", "World")); // Hello World를 출력
+        System.out.println(a.substring(0, 4)); // Hell 출력 -> 0~4번에 위치한 문자열 추출
     }
 }
