f3bfe0183639382371d5b546c0c5e57a6764d1fe
TAEHA117 <nn_goods@naver.com>
2024-02-15 16:30:16 +0900

    Test

---
+++ src/page_03/stage_4/Sample_58.java
@@ -0,0 +1,15 @@
+package page_03.stage_4;
+
+public class Sample_58 {
+    public static void main(String[] args) {
+
+        /*String a = "Happy Java";
+        String b = "a";
+        String c = "123";*/
+
+        String a = new String("Happy Java");
+        String b = new String("a");
+        String c = new String("123");
+
+    }
+}
