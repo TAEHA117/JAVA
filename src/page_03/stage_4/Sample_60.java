00a9d2be61ff45b954d7b6335b7dfc0546ff734a
TAEHA117 <nn_goods@naver.com>
2024-02-15 16:55:15 +0900

    Test

---
+++ src/page_03/stage_4/Sample_60.java
@@ -0,0 +1,17 @@
+package page_03.stage_4;
+
+public class Sample_60 {
+    public static void main(String[] args) {
+
+        String a = "hello";
+        String b = "java";
+        String c = "hello";
+        System.out.println(a.equals(b)); // false 출력
+        System.out.println(a.equals(c)); // true 출력
+
+/*        String a = "hello";
+        String b = new String("hello");
+        System.out.println(a.equals(b)); // true
+        System.out.println(a == b); // false*/
+    }
+}
