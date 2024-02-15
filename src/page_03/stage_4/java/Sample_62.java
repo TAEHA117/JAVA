d85d33decad9e7d716f3a0508c140f45d074c31b
TAEHA117 <nn_goods@naver.com>
2024-02-15 17:54:50 +0900

    Test

--- src/page_03/stage_4/java/Sample_62.java
+++ src/page_03/stage_4/java/Sample_62.java
@@ -5,7 +5,10 @@ public class Sample_62 {
         /*String a = "Hello Java";
         System.out.println(a.replaceAll("Java", "World")); // Hello World를 출력*/
 
+        /*String a = "Hello Java";
+        System.out.println(a.substring(0, 4)); // Hell 출력 -> 0~4번에 위치한 문자열 추출*/
+
         String a = "Hello Java";
-        System.out.println(a.substring(0, 4)); // Hell 출력 -> 0~4번에 위치한 문자열 추출
+        System.out.println(a.toUpperCase()); // HELLO JAVA 출력 - 대문자
     }
 }
---
+++ src/page_03/stage_4/java/Sample_63.java
@@ -0,0 +1,12 @@
+package page_03.stage_4.java;
+
+public class Sample_63 {
+    public static void main(String[] args) {
+        String a = "a:b:c:d";
+        String[] result = a.split(":"); // result는 {"a", "b", "c", "d"}
+
+        System.out.println(a);
+
+        System.out.println(String.format("I eat %d apples.", 3)); // " I eat 3 apples." 출력
+    }
+}
