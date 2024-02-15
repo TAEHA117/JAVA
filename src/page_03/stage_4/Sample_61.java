e05153139ef4b6d2495adf36166f109d5a913b64
TAEHA117 <nn_goods@naver.com>
2024-02-15 17:07:53 +0900

    Test

---
+++ src/page_03/stage_4/Sample_61.java
@@ -0,0 +1,16 @@
+package page_03.stage_4;
+
+public class Sample_61 {
+    public static void main(String[] args) {
+        /*String a = "Hello Java";
+        System.out.println(a.indexOf("Java")); // 6출력*/
+
+        /*String a = "Hello Java";
+        System.out.println(a.contains("Java")); // true*/
+
+        String a = "Hello Java";
+        System.out.println(a.charAt(6)); // J - 6번째의 위치한 J
+    }
+
+
+}
