172b3bc75251fa55a7dd242123e504a200885190
TAEHA117 <nn_goods@naver.com>
2024-02-15 15:01:27 +0900

    Test

---
+++ src/page_03/stage_1/Sample_52.java
@@ -0,0 +1,15 @@
+package page_03.stage_1;
+
+public class Sample_52 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a+b); // 15
+        System.out.println(a-b); // 5
+        System.out.println(a*b); // 50
+        System.out.println(a/b); // 2
+
+        System.out.println(7 % 3); // 1
+        System.out.println(3 % 7); // 3
+    }
+}
