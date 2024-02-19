21479f0ed708bd4d9f407cf4c3ad0665c9f39c8f
TAEHA117 <nn_goods@naver.com>
2024-02-19 13:58:17 +0900

    오류 확인 추후 수정

---
+++ src/page_03/stage_6/java/Sample_74.java
@@ -0,0 +1,8 @@
+package page_03.stage_6.java;
+
+public class Sample_74 {
+    public static void main(String[] args) {
+        String[] weeks = {"월","화","수","목","금","토","일"};
+        System.out.println(weeks[3]); // 목 - 0~3인덱스 -> 길이4
+    }
+}
---
+++ src/page_03/stage_6/java/Sample_75.java
@@ -0,0 +1,10 @@
+package page_03.stage_6.java;
+
+public class Sample_75 {
+    public static void main(String[] args) {
+        String[] weeks = {"월","화","수","목","금","토","일"};
+        for (int i=0; i<weeks.length; i++) {
+            System.out.println(weeks[i]); // 월화수목금토일
+        }
+    }
+}
