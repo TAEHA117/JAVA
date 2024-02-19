e9593fb7590c374a1dd888c22f94ad899267f7bd
TAEHA117 <nn_goods@naver.com>
2024-02-19 12:36:50 +0900

    이미지 오류 있음 -> 추후 수정 -> img테스트

---
+++ src/page_03/stage_4/java/Sample_66.java
@@ -0,0 +1,8 @@
+package page_03.stage_4.java;
+
+public class Sample_66 {
+    public static void main(String[] args) {
+        System.out.println(String.format("I have %s apples.",3)); // I have 3 apples. 출력
+        System.out.println(String.format("rate is %s.", 3.234)); // rate is 3.234. 출력
+    }
+}
