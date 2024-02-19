df67cf52a30ebb214e2e2af8ddaae4364ed1346a
TAEHA117 <nn_goods@naver.com>
2024-02-19 12:50:45 +0900

    오류 확인 추후 수정

---
+++ src/page_03/stage_4/java/Sample_67.java
@@ -0,0 +1,9 @@
+package page_03.stage_4.java;
+
+public class Sample_67 {
+    public static void main(String[] args) {
+//        System.out.println(String.format("%10s","hi.")); // "          hi."  -> 출력
+
+        System.out.println(String.format("%-10sjane.","hi")); // "hi          jane."  -> 출력
+    }
+}
