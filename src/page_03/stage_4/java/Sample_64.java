d05e52b88a68958f1a48e158ac6acf75a4d5e321
TAEHA117 <nn_goods@naver.com>
2024-02-19 12:28:19 +0900

    Test

---
+++ src/page_03/stage_4/java/Sample_64.java
@@ -0,0 +1,11 @@
+package page_03.stage_4.java;
+
+public class Sample_64 {
+    public static void main(String[] args) {
+//        System.out.println(String.format("I eat %s apples.", "five")); // I eat five apples 출력.
+
+        int number = 3;
+        System.out.println(String.format("I eat %d apples.", number)); // I eat 3 apples;
+
+    }
+}
---
+++ src/page_03/stage_4/java/Sample_65.java
@@ -0,0 +1,10 @@
+package page_03.stage_4.java;
+
+public class Sample_65 {
+    public static void main(String[] args) {
+        int number = 10;
+        String day = "three";
+        System.out.println(String.format("I ate %d apples. So I was sick for %s days.",number,day));
+        // I ate 10 apples. So I was sick for three days.
+    }
+}
